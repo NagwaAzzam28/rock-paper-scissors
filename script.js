3f671962917a9ef4a6773cd5d12b4585575540ee
nazzam28 <nagwaazzam28@gmail.com>
2026-07-15 00:18:40 +0300

    Create playround function

--- script.js
+++ script.js
@@ -9,11 +9,45 @@ function getComputerChoice (){
     return choice = "scissors"
    }
 }
-console.log(getComputerChoice())
 
 
 function getHumanChoice (user){
     return user = prompt("Enter your choice", "").toLowerCase()
 } 
-console.log(getHumanChoice())
 
+
+function playRound(humanChoice, computerChoice) {
+
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+
+    if (computerChoice === "rock" && humanChoice === "rock"){
+        return `computer choice is ${computerChoice} and you chose ${humanChoice} so it's a tie`
+    } else if (computerChoice === "rock" && humanChoice === "paper"){
+        return `computer choice is ${computerChoice} and you chose ${humanChoice} so you win!`
+    } else if (computerChoice === "rock" && humanChoice === "scissors"){
+        return `computer choice is ${computerChoice} and you chose ${humanChoice} so you lose`
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+        return `computer choice is ${computerChoice} and you chose ${humanChoice} so you lose`
+    } else if (computerChoice === "paper" && humanChoice === "paper") {
+        return `computer choice is ${computerChoice} and you chose ${humanChoice} so it's a tie`
+    } else if (computerChoice === "paper" && humanChoice === "scissors") {
+        return `computer choice is ${computerChoice} and you chose ${humanChoice} so you win`
+    } else if (computerChoice === "scissors" && humanChoice === "rock") {
+        return `computer choice is ${computerChoice} and you chose ${humanChoice} so you win`
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+        return `computer choice is ${computerChoice} and you chose ${humanChoice} so you lose`
+    } else if (computerChoice === "scissors" && humanChoice === "scissors") {
+        return `computer choice is ${computerChoice} and you chose ${humanChoice} so it's a tie`
+    } else {
+        return "Make sure you enter a valid choice"
+    }
+
+}
+console.log(playRound());
+console.log(playRound())
+
+
+
+// console.log("human choice is " + humanChoice)
+// console.log("computer choice is " + computerChoice)
