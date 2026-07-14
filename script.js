bc9e4e80eb05c194e34ca19626414b104512d2a9
nazzam28 <nagwaazzam28@gmail.com>
2026-07-14 23:23:41 +0300

    Add the computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("bism allah")
+function getComputerChoice (){
+   let choice =  Math.floor(Math.random() * 3 + 1)
+
+   if (choice === 1){
+    return choice = "Rock"
+   } else if (choice === 2){
+    return choice = "paper"
+   } else {
+    return choice = "scissors"
+   }
+}
+
+
+
+console.log(getComputerChoice())
