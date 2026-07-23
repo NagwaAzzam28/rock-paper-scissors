8b29c288f9fba8a47f65d83a8e60fe6e812d2c0d
nazzam28 <nagwaazzam28@gmail.com>
2026-07-23 23:42:04 +0300

    Add function for icons and Change final message location

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
 const rockBtn = document.querySelector("#rock");
-const paperkBtn = document.querySelector("#paper");
+const paperBtn = document.querySelector("#paper");
 const scissorsBtn = document.querySelector("#scissors");
+
 const playerScore = document.querySelector("#player-score");
 const compScore = document.querySelector("#computer-score");
 const roundResult = document.querySelector("#round-result");
 const playerRoundChoice = document.querySelector("#player-round-choice");
 const computerRoundChoice = document.querySelector("#computer-round-choice");
-const finalResult = document.querySelector("#final-result");
 const playAgainBtn = document.querySelector("#reset");
+const gameMessage = document.querySelector(".message");
+const orginalMessage = gameMessage.textContent;
 
 playAgainBtn.addEventListener("click", () => {
   rockBtn.disabled = false;
-  paperkBtn.disabled = false;
+  paperBtn.disabled = false;
   scissorsBtn.disabled = false;
   humanScore = 0;
   computerScore = 0;
   playerScore.textContent = 0;
   compScore.textContent = 0;
-  finalResult.textContent = "";
   playAgainBtn.style.display = "none";
+  gameMessage.textContent = orginalMessage;
+  gameMessage.style.cssText = "";
 });
+
 // the human choice
 
 function getHumanChoice(btn) {
   btn.addEventListener("click", (e) => {
-    const choice = e.currentTarget.innerText.toLowerCase();
+    const choice = e.currentTarget
+      .querySelector("span")
+      .textContent.toLowerCase();
+    console.log(choice);
     playRound(choice);
   });
 }
 function setHumanEvents() {
   getHumanChoice(rockBtn);
-  getHumanChoice(paperkBtn);
+  getHumanChoice(paperBtn);
   getHumanChoice(scissorsBtn);
 }
 setHumanEvents();
@@ -51,8 +58,8 @@ let computerScore = 0;
 function playRound(humanChoice) {
   const computerChoice = getComputerChoice();
 
-  playerRoundChoice.textContent = humanChoice;
-  computerRoundChoice.textContent = computerChoice;
+  playerRoundChoice.innerHTML = getHtmlIcons(humanChoice);
+  computerRoundChoice.innerHTML = getHtmlIcons(computerChoice);
 
   if (
     (computerChoice === "rock" && humanChoice === "rock") ||
@@ -69,9 +76,11 @@ function playRound(humanChoice) {
     playerScore.textContent = humanScore;
 
     if (humanScore === 5) {
-      endGame("congratulation! You Won!");
+      endGame("congratulation, You Won!");
+      gameMessage.style.cssText =
+        "color: #7EE36D; font-size:22px; text-transform: uppercase; font-weight:500;";
     } else {
-      roundResult.textContent = "YOU WIN";
+      roundResult.textContent = "you";
     }
   } else if (
     (computerChoice === "rock" && humanChoice === "scissors") ||
@@ -82,20 +91,32 @@ function playRound(humanChoice) {
     compScore.textContent = computerScore;
 
     if (computerScore === 5) {
-      endGame("The computer won, Better luck next time");
+      endGame("The computer won, try again");
+      gameMessage.style.cssText =
+        "color: #FF646B; font-size:22px; text-transform: uppercase; font-weight:500;";
     } else {
-      roundResult.textContent = "COMPUTER WINS";
+      roundResult.textContent = "computer";
     }
   }
 }
 
 function endGame(message) {
   rockBtn.disabled = true;
-  paperkBtn.disabled = true;
+  paperBtn.disabled = true;
   scissorsBtn.disabled = true;
-  finalResult.textContent = message;
+  gameMessage.textContent = message;
   playAgainBtn.style.display = "block";
   playerRoundChoice.textContent = "";
   computerRoundChoice.textContent = "";
   roundResult.textContent = "";
+}
+
+function getHtmlIcons(choice) {
+  if (choice === "rock") {
+    return '<i class="fa-solid fa-hand-back-fist"></i>';
+  } else if (choice === "paper") {
+    return '<i class="fa-solid fa-hand"></i>';
+  } else if (choice === "scissors") {
+    return '<i class="fa-solid fa-hand-scissors"></i>';
+  }
 }
